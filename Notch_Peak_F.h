0fb8f405257ceaa1e907a02570cee46480a6c0bc
Ale Presacco <pressalex@hotmail.com>
2023-12-07 11:39:21 -0800

    Update Notch_Peak_F.h
    Adding the option to choose the dB level of the bandwidth

--- Notch_Peak_F.h
+++ Notch_Peak_F.h
@@ -21,8 +21,10 @@ extern "C" {  // only need to export C interface if
 
             //Estimate the coeffients of a band-pass filter and return a 2 rows x N coefficients matrix. Row 1 = Numerator; Row 2 = Denumerator
             std::vector<std::vector<double> > iirnotch_cpp(double, double); //Notch filter
+            std::vector<std::vector<double> > iirnotch_cpp(double, double, double); //Overload of the Notch filter allowing the user to specify the dB level of the bandwidth
 
             std::vector<std::vector<double> > iircomb_cpp(double, double, std::string); //Comb or peak filter
+            std::vector<std::vector<double> > iircomb_cpp(double, double, double, std::string); //Overload of the Comb or peak filter allowing the user to specify the dB level of the bandwidth
 
             //Filter the data by using the Direct-Form II Transpose, as explained in the Matlab documentation
             std::vector<double> Filter_Data_NCP(std::vector<std::vector<double> >, std::vector<double>);
